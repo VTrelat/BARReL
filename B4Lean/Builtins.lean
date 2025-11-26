b80c741e3a827b7cc01d59f857fa3771d9e47f59
VTrelat <vincent.trelat@depinfonancy.net>
2025-11-26 13:04:29 +0100

    updates

--- B4Lean/Builtins.lean
+++ B4Lean/Builtins.lean
@@ -4,11 +4,8 @@ import B.Syntax.Extra
 
 namespace B.Builtins
 
-abbrev MAXINT : ℤ := B.MAXINT
-abbrev MININT : ℤ := B.MININT
-
-abbrev NAT : Set ℤ := { n | 0 ≤ n ∧ n ≤ MAXINT }
-abbrev NAT1 : Set ℤ := { n | 1 ≤ n ∧ n ≤ MAXINT }
+abbrev NAT : Set ℤ := { n | 0 ≤ n ∧ n ≤ B.MAXINT }
+abbrev NAT1 : Set ℤ := { n | 1 ≤ n ∧ n ≤ B.MAXINT }
 abbrev NATURAL : Set ℤ := { n | 0 ≤ n }
 abbrev NATURAL1 : Set ℤ := { n | 1 ≤ n }
 
@@ -18,37 +15,11 @@ abbrev FLOAT : Set Float := Set.univ
 
 abbrev REAL : Set ℝ := Set.univ
 
-def varIsReserved : String → Prop
-  | "NAT" | "NAT1" | "NATURAL" | "NATURAL1"
-  | "INT"
-  | "FLOAT"
-  | "REAL"
-    => True
-  | _ => False
-
-instance : DecidablePred varIsReserved := by
-  intro v
-  unfold varIsReserved
-  split <;>
-  first
-  | exact instDecidableTrue
-  | exact instDecidableFalse
-
 def interval (lo hi : ℤ) : Set ℤ := { n | lo ≤ n ∧ n ≤ hi }
 
 infix:50 ".." => interval
 
 
 end Builtins
-
-open Lean Elab Builtins
-
-def reservedVarToExpr : String → TermElabM Lean.Expr
-  | "NAT" => return mkConst ``NAT
-  | "NAT1" => return mkConst ``NAT1
-  | "NATURAL" => return mkConst ``NATURAL
-  | "NATURAL1" => return mkConst ``NATURAL1
-  | "INT" => return mkConst ``INT
-  | v => throwError "Variable {v} is not reserved."
 
 end B
